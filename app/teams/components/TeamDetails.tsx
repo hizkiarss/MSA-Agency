7d300883650490ea136b55ff9c44b4037b86553c
qakaben <hizkiarssihombing@gmail.com>
2024-05-22 15:22:51 +0700

    adding suspense in the team details to improve cls

--- app/teams/components/TeamDetails.tsx
+++ app/teams/components/TeamDetails.tsx
@@ -2,7 +2,7 @@ import { GoLocation } from "react-icons/go"
 import { cn } from "@/utils/cn"
 import { AnimatePresence, motion } from "framer-motion"
 import Link from "next/link"
-import { useState } from "react"
+import { useState, Suspense } from "react"
 import Image from "next/image"
 import Cards from "@/components/Card"
 
@@ -20,80 +20,90 @@ export const TeamDetails = ({
 }) => {
   let [hoveredIndex, setHoveredIndex] = useState<number | null>(null)
 
+  const SkeletonLoader = () => (
+    <div className='h-56 w-full bg-gray-200'>
+      <div className='h-4 w-3/4 bg-gray-300 my-2 mx-auto rounded'></div>
+      <div className='h-4 w-2/4 bg-gray-300 my-2 mx-auto rounded'></div>
+      <div className='h-4 w-1/4 bg-gray-300 my-2 mx-auto rounded'></div>
+    </div>
+  )
+
   return (
-    <Cards>
-      <div
-        className={cn(
-          "grid grid-cols md:grid-cols-3 md:px-[100px] mt-7 md:mt-0 md:py-10 mb-[100px]",
-          className
-        )}
-      >
-        {items.map((item, idx) => (
-          <div className='flex justify-center' key={idx}>
-            <Link
-              href={item?.link}
-              key={item?.link}
-              className='relative group block p-2 h-full w-10/12 md:w-full'
-              onMouseEnter={() => setHoveredIndex(idx)}
-              onMouseLeave={() => setHoveredIndex(null)}
-            >
-              <AnimatePresence>
-                {hoveredIndex === idx && (
-                  <motion.span
-                    className='absolute inset-0 h-full w-full bg-[#e0e0e0] dark:bg-slate-800/[0.8] block  rounded-3xl'
-                    layoutId='hoverBackground'
-                    initial={{ opacity: 0 }}
-                    animate={{
-                      opacity: 1,
-                      transition: { duration: 0.15 },
-                    }}
-                    exit={{
-                      opacity: 0,
-                      transition: { duration: 0.15, delay: 0.2 },
-                    }}
-                  />
-                )}
-              </AnimatePresence>
-              <Card className=''>
-                <Cards>
-                  <div className='flex gap-3 justify-center md:gap-0 md:grid md:grid-rows md:grid-rows-3 '>
-                    <div className='md:row-span-2'>
-                      <Image
-                        src={item.image}
-                        alt={item.title}
-                        width={50}
-                        height={50}
-                        objectFit='cover'
-                        className='rounded-xl h-[75px] w-[100px] md:h-[300px] md:!w-full object-cover'
-                      />
-                    </div>
-                    <div className='flex flex-col gap-2 md:gap-0 md:row-span-1 md:grid md:grid-rows-2'>
-                      <CardTitle className='font-[400] text-[15px] md:text-[40px] text-black tracking-tight row-span-1'>
-                        {item.title}
-                      </CardTitle>
-                      <div className='row-span-1 '>
-                        <div className='flex items-center gap-[8px] mb-2'>
-                          <GoLocation className='text-gray-400' size={20} />
-                          <CardDescription className='text-[12px] md:text-[20px] tracking-tight'>
-                            {item.description}
-                          </CardDescription>
-                        </div>
-                        <div className='flex justify-start'>
-                          {" "}
-                          <p className='tracking-tight text-[10px] md:text-[14px] break-words w-full'>
-                            {item.link}
-                          </p>
+    <Suspense fallback={<SkeletonLoader />}>
+      <Cards>
+        <div
+          className={cn(
+            "grid grid-cols md:grid-cols-3 md:px-[100px] mt-7 md:mt-0 md:py-10 mb-[100px]",
+            className
+          )}
+        >
+          {items.map((item, idx) => (
+            <div className='flex justify-center' key={idx}>
+              <Link
+                href={item?.link}
+                key={item?.link}
+                className='relative group block p-2 h-full w-10/12 md:w-full'
+                onMouseEnter={() => setHoveredIndex(idx)}
+                onMouseLeave={() => setHoveredIndex(null)}
+              >
+                <AnimatePresence>
+                  {hoveredIndex === idx && (
+                    <motion.span
+                      className='absolute inset-0 h-full w-full bg-[#e0e0e0] dark:bg-slate-800/[0.8] block  rounded-3xl'
+                      layoutId='hoverBackground'
+                      initial={{ opacity: 0 }}
+                      animate={{
+                        opacity: 1,
+                        transition: { duration: 0.15 },
+                      }}
+                      exit={{
+                        opacity: 0,
+                        transition: { duration: 0.15, delay: 0.2 },
+                      }}
+                    />
+                  )}
+                </AnimatePresence>
+                <Card className=''>
+                  <Cards>
+                    <div className='flex gap-3 justify-center md:gap-0 md:grid md:grid-rows md:grid-rows-3 '>
+                      <div className='md:row-span-2'>
+                        <Image
+                          src={item.image}
+                          alt={item.title}
+                          width={50}
+                          height={50}
+                          objectFit='cover'
+                          className='rounded-xl h-[75px] w-[100px] md:h-[300px] md:!w-full object-cover'
+                        />
+                      </div>
+                      <div className='flex flex-col gap-2 md:gap-0 md:row-span-1 md:grid md:grid-rows-2'>
+                        <CardTitle className='font-[400] text-[15px] md:text-[40px] text-black tracking-tight row-span-1'>
+                          {item.title}
+                        </CardTitle>
+                        <div className='row-span-1 '>
+                          <div className='flex items-center gap-[8px] mb-2'>
+                            <GoLocation className='text-gray-400' size={20} />
+                            <CardDescription className='text-[12px] md:text-[20px] tracking-tight'>
+                              {item.description}
+                            </CardDescription>
+                          </div>
+                          <div className='flex justify-start'>
+                            {" "}
+                            <p className='tracking-tight text-[10px] md:text-[14px] break-words w-full'>
+                              {item.link}
+                            </p>
+                          </div>
                         </div>
                       </div>
                     </div>
-                  </div>
-                </Cards>
-              </Card>
-            </Link>
-          </div>
-        ))}
-      </div>
-    </Cards>
+                  </Cards>
+                </Card>
+              </Link>
+            </div>
+          ))}
+        </div>
+      </Cards>
+    </Suspense>
   )
 }
 
